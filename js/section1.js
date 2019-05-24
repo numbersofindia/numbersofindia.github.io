58c91014dbc54e4ef22a2d6219102b2ca827ecca
Vivek Aithal <vivekaithal44@gmail.com>
2019-05-24 20:29:08 +0530

    [Vivek] add leaflet support

--- js/section1.js
+++ js/section1.js
@@ -16,54 +16,36 @@ function section1() {
         initialViewState: INITIAL_VIEW_STATE,
         controller: true,
         layers: [
-            new TileLayer({
-                opacity: 0.1,
-                minZoom: 0,
-                maxZoom: 19,
-                renderSubLayers: (props) => {
-                    // console.log(props);
-                    const { x, y, z, bbox } = props.tile;
-                    const { west, south, east, north } = bbox;
+            // new TileLayer({
+            //     opacity: 0.1,
+            //     minZoom: 0,
+            //     maxZoom: 19,
+            //     renderSubLayers: (props) => {
+            //         // console.log(props);
+            //         const { x, y, z, bbox } = props.tile;
+            //         const { west, south, east, north } = bbox;
 
-                    return new BitmapLayer(props, {
-                        // image: `${"https://abcd.basemaps.cartocdn.com/dark_nolabels"}/${z}/${x}/${y}.png`, //Carto BW with labels
-                        image: `${"https://abcd.basemaps.cartocdn.com/light_nolabels"}/${z}/${x}/${y}.png`, //Carto BW with labels
-                        // image: `${"https://stamen-tiles-c.a.ssl.fastly.net/toner-lines"}/${z}/${x}/${y}@2x.png`, //Carto BW with labels
-                        // image: `${"https://stamen-tiles-abcd.a.ssl.fastly.net/toner-background"}/${z}/${x}/${y}@2x.png`, //Stamen BW with labels
-                        // image: `${"https://abcd.basemaps.cartocdn.com/rastertiles/voyager_nolabels"}/${z}/${x}/${y}.png`, //Voyager no labels  with labels
-                        // image: `${"https://abcd.basemaps.cartocdn.com/light_nolabels/"}/${z}/${x}/${y}.png`, //ESRI physical no labels  with labels
-                        bounds: [west, south, east, north]
-                    })
-                },
-                // getTileData: ({x, y, z}) => {
-                //     const mapSource = `https://abcd.basemaps.cartocdn.com/dark_nolabels/${z}/${x}/${y}.vector.pbf`;
-                //     return fetch(mapSource)
-                //       .then(response => response.arrayBuffer())
-                //       .then(buffer => {
-                //         const tile = new VectorTile(new Pbf(buffer));
-                //         const features = [];
-                //         for (const layerName in tile.layers) {
-                //           const vectorTileLayer = tile.layers[layerName];
-                //           for (let i = 0; i < vectorTileLayer.length; i++) {
-                //             const vectorTileFeature = vectorTileLayer.feature(i);
-                //             const feature = vectorTileFeature.toGeoJSON(x, y, z);
-                //             features.push(feature);
-                //           }
-                //         }
-                //         return features;
-                //       });
-                // }
-            }),
+            //         return new BitmapLayer(props, {
+            //             // image: `${"https://abcd.basemaps.cartocdn.com/dark_nolabels"}/${z}/${x}/${y}.png`, //Carto BW with labels
+            //             image: `${"https://abcd.basemaps.cartocdn.com/light_nolabels"}/${z}/${x}/${y}.png`, //Carto BW with labels
+            //             // image: `${"https://stamen-tiles-c.a.ssl.fastly.net/toner-lines"}/${z}/${x}/${y}@2x.png`, //Carto BW with labels
+            //             // image: `${"https://stamen-tiles-abcd.a.ssl.fastly.net/toner-background"}/${z}/${x}/${y}@2x.png`, //Stamen BW with labels
+            //             // image: `${"https://abcd.basemaps.cartocdn.com/rastertiles/voyager_nolabels"}/${z}/${x}/${y}.png`, //Voyager no labels  with labels
+            //             // image: `${"https://abcd.basemaps.cartocdn.com/light_nolabels/"}/${z}/${x}/${y}.png`, //ESRI physical no labels  with labels
+            //             bounds: [west, south, east, north]
+            //         })
+            //     },
+            // }),
             new GeoJsonLayer({
                 data: INDIA_GEOJSON,
                 stroked: true,
                 filled: true,
                 lineWidthMinPixels: 0.4,
-                opacity: 0.6,
+                opacity: 0.4,
                 getLineColor: [255, 255, 255],
-                // getFillColor: [22, 168, 175, 255]
+                getFillColor: [22, 168, 175, 255]
                 // getFillColor: [192, 57, 43, 255]
-                getFillColor: [243, 158, 18, 255]
+                // getFillColor: [243, 158, 18, 255]
             }),
             
         ]
--- js/section2.js
+++ js/section2.js
@@ -1,3 +1,12 @@
-function section2(){
-    console.log('Section2');
+function section2() {
+    var mymap = L.map('leaflet-map',{
+        center: [23.2493, 77.5512],
+        zoom: 6
+    });
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(mymap);
+    
 }
